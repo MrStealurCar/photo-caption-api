dd599f1c4408cfb6896cbb33161d9d7681e8b859
Jacob Rodriguez <jacob_rod1231@outlook.com>
2026-01-14 10:51:43 -0500

    chore(app): import route files and set up routers

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const userRouter = require("./routes/users.js");
+const captionRouter = require("./routes/captions.js");
+const imageRouter = require("./routes/images.js");
 // Middleware to parse JSON bodies
 app.use(express.json());
-
-// Sample route
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/images", imageRouter);
+app.use("/captions", captionRouter);
+app.use("/users", userRouter);
 
 // Starts the server
 app.listen(port, () => {
